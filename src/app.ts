ff232c0315b5f6af3a7065926beeae425e29b953
imran hossain <imranme.global@gmail.com>
2026-06-23 16:10:41 +0600

    express basics middlwares use

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,19 @@
 import exprese, { Application, Request, Response } from "express";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+import config from "./config";
 
 const app: Application = exprese()
+
+app.use(cors({
+    origin: config.app_url,
+    credentials: true
+}))
+
+app.use(exprese.json());
+app.use(exprese.urlencoded({ extended: true }))
+app.use(cookieParser())
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Prisma Press Backend App Running....')
