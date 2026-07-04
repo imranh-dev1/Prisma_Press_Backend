4bbaa688b92280e3e2d9ed388e7515c87ca67258
Imran Hossain <imranhossen0586@gmail.com>
2026-07-04 17:47:11 +0600

    Checkout session created successfully & checkout api

--- src/modules/subscription/subscription.controller.ts
+++ src/modules/subscription/subscription.controller.ts
@@ -1,8 +1,19 @@
 import { NextFunction, Request, Response } from "express"
 import { catchAsync } from "../../utils/catchAsync"
+import { subscriptionServices } from "./subscription.service";
+import { sendResponse } from "../../utils/sendResponse";
+import status from "http-status";
 
 const createCheckoutSession = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const userId = req.user?.id;
 
+    const result = await subscriptionServices.createCheckoutSession(userId as string);
+    sendResponse(res, {
+        success: true,
+        statusCode: status.OK,
+        message: "Checkout session created successfully.",
+        data: result
+    })
 })
 
 export const subscriptionControllers = {
--- src/modules/subscription/subscription.route.ts
+++ src/modules/subscription/subscription.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { subscriptionControllers } from "./subscription.controller";
+import { auth } from "../../middlewares/auth";
+import { Role } from "../../../generated/prisma/enums";
 
 const router = Router();
 
-router.post("/checkout", subscriptionControllers.createCheckoutSession)
+router.post("/checkout", auth(Role.ADMIN, Role.AUTHOR, Role.USER), subscriptionControllers.createCheckoutSession)
 
 export const subscriptionRoutes = router;
